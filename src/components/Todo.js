d541ca848c293e4d11af4e7d24c517387b5c84d9
psytb5 <psytb5@nottingham.ac.uk>
2019-06-06 11:18:57 +0100

    Replicating componentDidMount() with useEffect() hook

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Todo = props => {
     const [todoName, setTodoName] = useState('');
     const [todoList, setTodoList] = useState([]);
 
+    useEffect(() => {
+        axios
+            .get('https://react-hooks-95d0d.firebaseio.com/todos.json')
+            .then(result => {
+                console.log(result);
+                const todoData = result.data;
+                const todos = [];
+                for (const key in todoData) {
+                    todos.push({ id: key, name: todoData[key].name });
+                }
+                setTodoList(todos);
+            });
+    }, []);
     const inputChangeHandler = event => {
         setTodoName(event.target.value);
     };
 
-    const todoAddHandler = () => {
+    const todoAddHandler = async () => {
         setTodoList(todoList.concat(todoName));
+        try {
+            const res = await axios.post(
+                'https://react-hooks-95d0d.firebaseio.com/todos.json',
+                { name: todoName }
+            );
+            console.log(res);
+        } catch (err) {
+            console.log(err);
+        }
         setTodoName('');
     };
 
@@ -26,7 +49,7 @@ const Todo = props => {
             </button>
             <ul>
                 {todoList.map(todo => (
-                    <li key={todo}>{todo}</li>
+                    <li key={todo.id}>{todo.name}</li>
                 ))}
             </ul>
         </React.Fragment>
