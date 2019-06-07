23495de6ac22e3a616d3ebea44d83484ba65ad08
psytb5 <psytb5@nottingham.ac.uk>
2019-06-07 12:02:43 +0100

    Added fake authentication to try out the useContext hook- a cleaner way of using the context API

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AuthContext from '../auth-context';
 
-const Header = props => (
-    <header>
-        <button onClick={props.onLoadTodos}>Todo List</button> |{' '}
-        <button onClick={props.onLoadAuth}>Auth</button>
-    </header>
-);
+const Header = props => {
+    const auth = useContext(AuthContext);
+
+    return (
+        <header>
+            {auth.status ? (
+                <button onClick={props.onLoadTodos}>Todo List</button>
+            ) : null}
+
+            <button onClick={props.onLoadAuth}>Auth</button>
+        </header>
+    );
+};
 
 export default Header;
